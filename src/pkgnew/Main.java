366d72e0fd53bbbef537585842269cb5442ba328
NEVED123 <neved1234@gmail.com>
2022-06-07 11:46:48 -0400

    refactored code for readability

--- src/pkgnew/Main.java
+++ src/pkgnew/Main.java
@@ -2,8 +2,10 @@
 package pkgnew;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import javafx.application.Application;
@@ -24,9 +26,7 @@ public class Main extends Application {
     public static ArrayList<Task> doneList;
     public static ArrayList<Task> todayList;
     private static File directory;
-    public static File taskFile;
-    public static PrintWriter printWriter;
-
+    public String path;
     /**
      *
      * @param stage
@@ -42,56 +42,47 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         //create folder C:\FastTask
         directory = new File(System.getProperty("user.home") + "\\FastTask");
+        path = System.getProperty("user.home") + "\\FastTask\\fasttask.txt";
         if (!directory.exists()) {
-            if (directory.mkdir()) {
-                System.out.println("Directory is created!");
-                //not much else to do here, it is a blank folder
-                try{
-                    printWriter = new PrintWriter(System.getProperty("user.home") + "\\FastTask\\fasttask.txt");
-                    taskFile = new File(System.getProperty("user.home") + "\\FastTask\\fasttask.txt"); 
-                }
-                catch(Exception e){
-                    e.getStackTrace();
-                }        
-            } else {
-                System.out.println("Directory failed to create");
-                //add code in here that creates an error
+            try{
+                directory.mkdir();
+            }
+            catch(Exception e){
+                e.getStackTrace();
+            }
+        }       
+        File file = new File(path);
+        if(!file.exists()){
+            try{
+                FileWriter taskWriter = new FileWriter(path, true);
+            }
+            catch(Exception e){
+                e.getStackTrace();
             }
         }
-        else{
-            File file = new File(System.getProperty("user.home") + "\\FastTask\\fasttask.txt");
-            if(!file.exists()){
-                printWriter = new PrintWriter(System.getProperty("user.home") + "\\FastTask\\fasttask.txt"); //creates txt file if it is not there
-                taskFile = new File(System.getProperty("user.home") + "\\FastTask\\fasttask.txt");
+
+        todoList = new ArrayList<>();
+        doingList = new ArrayList<>();
+        doneList = new ArrayList<>();
+        todayList = new ArrayList<>();
+        //we have a the FastTask folder and an existing txt file, now we must load info from the document
+        HashMap<String, ArrayList<Task>> columnKey = new HashMap();
+        columnKey.put("todo", todoList);
+        columnKey.put("doing", doingList);
+        columnKey.put("done", doneList);
+        columnKey.put("today", todayList);
+        ArrayList<String> taskStrings = (ArrayList)Files.readAllLines(Paths.get(path));
+        for(String taskString : taskStrings){
+            String[] split = taskString.split(",");
+            if(split.length == 5 && columnKey.containsKey(split[0])){
+                //System.out.println(columnKey.get(split[0]));
+                columnKey.get(split[0]).add(new Task(split[1], split[2], split[3], split[4]));
             }
-            else{ 
-                taskFile = new File(System.getProperty("user.home") + "\\FastTask\\fasttask.txt"); 
-                todoList = new ArrayList<>();
-                doingList = new ArrayList<>();
-                doneList = new ArrayList<>();
-                todayList = new ArrayList<>();
-                //we have a the FastTask folder and an existing txt file, now we must load info from the document
-                HashMap<String, ArrayList<Task>> columnKey = new HashMap();
-                columnKey.put("todo", todoList);
-                columnKey.put("doing", doingList);
-                columnKey.put("done", doneList);
-                columnKey.put("today", todayList);
-                ArrayList<String> taskStrings = (ArrayList)Files.readAllLines(taskFile.toPath());
-                for(String taskString : taskStrings){
-                    String[] split = taskString.split(",");
-                    /*System.out.println(split.length);
-                    for(String i : split){
-                        System.out.println(i);
-                    }*/
-                    if(split.length == 5 && columnKey.containsKey(split[0])){
-                        //System.out.println(columnKey.get(split[0]));
-                        columnKey.get(split[0]).add(new Task(split[1], split[2], split[3], split[4]));
-                    }
-                    //else System.out.println(split.length); //ELSE STATEMENT FOR DEBUG
-                }
-                
-            }
+            //else System.out.println(split.length); //ELSE STATEMENT FOR DEBUG
         }
+
+        
+        
  
         //fasttask.txt
         //if there's already the folder, load the information from it 
@@ -99,22 +90,13 @@ public class Main extends Application {
         root = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
         
         FlowPane todoFlowPane = (FlowPane)root.lookup("#todo");
-        /*FlowPane doingFlowPane = (FlowPane)root.lookup("doing");
-        FlowPane doneFlowPane = (FlowPane)root.lookup("#done");
-        FlowPane todayFlowPane = (FlowPane)root.lookup("#today");*/
+        //FlowPane doingFlowPane = (FlowPane)root.lookup("doing");
+        //FlowPane doneFlowPane = (FlowPane)root.lookup("#done");
+        //FlowPane todayFlowPane = (FlowPane)root.lookup("#today");
         
         for(Task task : todoList){
             todoFlowPane.getChildren().add(task.getTask());
         }
-        /*for(Task task : doingList){
-            doingFlowPane.getChildren().add(task.getTask());
-        }
-        for(Task task : doneList){
-            doneFlowPane.getChildren().add(task.getTask());
-        }
-        for(Task task : todayList){
-            todayFlowPane.getChildren().add(task.getTask());
-        }*/
         
         Scene scene = new Scene(root);
         
